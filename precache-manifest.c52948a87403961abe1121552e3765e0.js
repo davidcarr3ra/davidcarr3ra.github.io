3aa01384c4cb5a4e66106d0241ffbbbf63867f2e
David Carrera <davidcarrera@Davids-MacBook-Pro-3.local>
2020-08-19 23:41:19 -0700

    Updates

---
+++ precache-manifest.c52948a87403961abe1121552e3765e0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37324f8d821618adf903709f7e3fadd2",
+    "url": "/davidcarr3ra.github.io/index.html"
+  },
+  {
+    "revision": "f862b31ac447cdcc5e75",
+    "url": "/davidcarr3ra.github.io/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "c6c7178550b1de4b5217",
+    "url": "/davidcarr3ra.github.io/static/css/main.297747fa.chunk.css"
+  },
+  {
+    "revision": "f862b31ac447cdcc5e75",
+    "url": "/davidcarr3ra.github.io/static/js/2.87220242.chunk.js"
+  },
+  {
+    "revision": "c9b31f27ce0803bcbda04bfd0eba249e",
+    "url": "/davidcarr3ra.github.io/static/js/2.87220242.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6c7178550b1de4b5217",
+    "url": "/davidcarr3ra.github.io/static/js/main.d90f9de6.chunk.js"
+  },
+  {
+    "revision": "7a6f0d9d564380ca551f",
+    "url": "/davidcarr3ra.github.io/static/js/runtime-main.af803f48.js"
+  }
+]);

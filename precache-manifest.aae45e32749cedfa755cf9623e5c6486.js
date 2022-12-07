68946b3b98ef4dfe8f929ef4261e6f824d9569f7
David Carrera <carrera.david99@gmail.com>
2022-12-07 16:48:00 -0500

    Updates

---
+++ precache-manifest.aae45e32749cedfa755cf9623e5c6486.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c52620c04a88df9dc4ffdd0cfb018bc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ee7cf535a8ac6bf09af8",
+    "url": "/static/css/2.85c36af2.chunk.css"
+  },
+  {
+    "revision": "3cc78ce834829ae1e4d3",
+    "url": "/static/css/main.98c0de1f.chunk.css"
+  },
+  {
+    "revision": "ee7cf535a8ac6bf09af8",
+    "url": "/static/js/2.049c289b.chunk.js"
+  },
+  {
+    "revision": "1384e44c886df90ff602213558ebdf55",
+    "url": "/static/js/2.049c289b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3cc78ce834829ae1e4d3",
+    "url": "/static/js/main.58b7175a.chunk.js"
+  },
+  {
+    "revision": "3860be2213f26bfbc743",
+    "url": "/static/js/runtime-main.9150b686.js"
+  },
+  {
+    "revision": "12e3b08d05abb034336ddb986853a3b8",
+    "url": "/static/media/bloomberg.12e3b08d.png"
+  },
+  {
+    "revision": "2d34462b5f254aa53bcc875990b41ab2",
+    "url": "/static/media/palantir.2d34462b.png"
+  }
+]);

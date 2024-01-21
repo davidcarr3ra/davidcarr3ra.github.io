29305bd8c25ca4711110e55771d1c1556951a234
David Carrera <carrera.david99@gmail.com>
2024-01-20 20:49:54 -0500

    Updates

--- precache-manifest.78668f7d63635085bd22bafb5ea1c454.js
+++ precache-manifest.78668f7d63635085bd22bafb5ea1c454.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "7a2caf05ea0ab32a87081e4f99c3679b",
+    "revision": "fef4b60821965842aef3c87c62401939",
     "url": "/index.html"
   },
   {
-    "revision": "828a0beeefbf9ae6491f",
+    "revision": "4c1dde198150deba1be2",
     "url": "/static/css/2.a94b31b9.chunk.css"
   },
   {
-    "revision": "ffcb68626171d0e20425",
-    "url": "/static/css/main.a48672c4.chunk.css"
+    "revision": "d3d24dceb85b50482730",
+    "url": "/static/css/main.73fe0b45.chunk.css"
   },
   {
-    "revision": "828a0beeefbf9ae6491f",
-    "url": "/static/js/2.c68a124b.chunk.js"
+    "revision": "4c1dde198150deba1be2",
+    "url": "/static/js/2.d824a2f7.chunk.js"
   },
   {
     "revision": "6c55044eaf53ab9170ae44a04090f3d5",
-    "url": "/static/js/2.c68a124b.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.d824a2f7.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "ffcb68626171d0e20425",
-    "url": "/static/js/main.407be551.chunk.js"
+    "revision": "d3d24dceb85b50482730",
+    "url": "/static/js/main.3fd39bfe.chunk.js"
   },
   {
     "revision": "3860be2213f26bfbc743",
@@ -30,6 +30,10 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "12e3b08d05abb034336ddb986853a3b8",
     "url": "/static/media/bloomberg.12e3b08d.png"
+  },
+  {
+    "revision": "a0c37c69cb9de69d4e0b2ecc429e168d",
+    "url": "/static/media/ethglobal.a0c37c69.svg"
   },
   {
     "revision": "2d34462b5f254aa53bcc875990b41ab2",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5dcb89cf5d82d1a74d99f607cc5429c9.js"
+  "/precache-manifest.78668f7d63635085bd22bafb5ea1c454.js"
 );
 
 self.addEventListener('message', (event) => {
